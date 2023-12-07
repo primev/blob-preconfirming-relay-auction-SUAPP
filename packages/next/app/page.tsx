f3651cfa6bfdc865f8cb2f02e620fe6a2a23eb41
andytudhope <13001517+andytudhope@users.noreply.github.com>
2023-12-07 18:44:41 +0200

    still not getting ccrs right

--- packages/next/app/page.tsx
+++ packages/next/app/page.tsx
@@ -1,19 +1,20 @@
 "use client";
-import React, { useState } from 'react';
-import { createWalletClient, http, serializeTransaction, hexToSignature, keccak256, parseGwei } from 'viem';
-import { suaveRigil } from 'viem/chains';
+import React, { useState, useEffect } from 'react';
+import { createWalletClient, custom, serializeTransaction, hexToSignature, keccak256, parseGwei, formatEther, encodeFunctionData } from 'viem';
+import { suaveRigil, localhost } from 'viem/chains';
 import { TransactionRequestSuave, SuaveTxTypes } from '../node_modules/viem/chains/suave/types'
-import { deployedAddress } from '@/constants/addresses';
+import { CCR } from './ccr';
+import { deployedAddress, kettleAddress } from '@/constants/addresses';
 import { abi } from '@/constants/abi';
 import Header from '@/components/Header';
 import Links from '@/components/Links';
 
 export default function Home() {
   const [account, setAccount] = useState(null);
-  const [balance, setBalance] = useState('');
+  const [balance, setBalance] = useState(0);
+  const [provider, setProvider] = useState({})
+  const [hash, setHash] = useState('')
   const [contractState, setContractState] = useState('');
-  const suaveUrl = 'http://localhost:8545';
-  const provider = suaveRigil.newPublicClient(http(suaveUrl));
 
   // needs to be set manually for CCRs using eth_sign method
   var nonce = 0;
@@ -23,34 +24,43 @@ export default function Home() {
           try {
               const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
               setAccount(account);
-              const balanceFetched = await provider.getBalance({ address: account });
-              setBalance(balanceFetched.toString());
+              const suaveProvider = suaveRigil.newPublicClient(custom(ethereum));
+              setProvider(suaveProvider);
           } catch (error) {
               console.error("Error connecting to wallet:", error);
           }
       } else {
-          console.log('Please install MetaMask!');
+          console.log('Please install a browser wallet');
       }
   };
+
+  useEffect(() => {
+    if (Object.keys(provider).length > 0) {
+      const fetchBalance = async () => {
+        const balanceFetched = await provider.getBalance({ address: account });
+        setBalance(formatEther(balanceFetched));
+      };
+
+      fetchBalance();
+    }
+  }, [account, hash]);
 
   const getFunds = async () => {
     // default funded key in local SUAVE devenv
     const privateKey = '0x91ab9a7e53c220e6210460b65a7a3bb2ca181412a8a7b43ff336b3df1737ce12';
-    const fundingWallet = suaveRigil.newWallet(privateKey, http(suaveUrl));
+    const fundingWallet = suaveRigil.newWallet({privateKey: privateKey, transport: custom(ethereum)});
     const fundTx = {
         to: account,
-        value: 1n,
-        type: '0x0',
+        value: 1000000000000000000n,
+        type: '0x0' as '0x0',
         gas: 21000n,
         gasPrice: 1000000000n,
     } as TransactionRequestSuave;
     const sendRes = await fundingWallet.sendTransaction(fundTx);
-    console.log(`Transaction hash: ${sendRes}`);
+    setHash(sendRes);
   }
 
-  // TODO: the issue with this approach is how to increment the nonce well
-  // if not specified, it causes errors like "already known" or "replacement transaction underpriced"
-  const sendChangeState = async () => {
+  const sendExample = async () => {
     const CCR: TransactionRequestSuave = {
       confidentialInputs: '0x',
       kettleAddress: '0xB5fEAfbDD752ad52Afb7e1bD2E40432A485bBB7F', // Use 0x03493869959C866713C33669cA118E774A30A0E5 on Rigil.
@@ -58,18 +68,18 @@ export default function Home() {
       gasPrice: parseGwei('0.0000000002'), 
       gas: parseGwei('0.0002'),
       type: SuaveTxTypes.ConfidentialRequest, 
-      chainId: 16813125, // chain id of local SUAVE devnet
-      data: '0x6fd43e7c00000000000000000000000000000000000000000000000000000000', // calling example()
+      chainId: 16813125, // chain id of local SUAVE devnet and Rigil
+      data: encodeFunctionData({
+        abi,
+        functionName: 'example',
+      }),
       nonce
     };
     if (account) {
       const wallet = createWalletClient({ 
         account, 
-        transport: http(suaveUrl)
+        transport: custom(ethereum)
       });
-      // We use the unsafe eth_sign method here due to the lack of support for CCR tx types in viem
-      // this requires users to enable this option in MetaMask's Advanced settings
-      // it is not a long term solution
       const serialized = serializeTransaction(CCR);
       const serializedHash = keccak256(serialized);
       const hexSignature = await (window as any).ethereum.request({ method: 'eth_sign', params: [account, serializedHash] });
@@ -83,34 +93,8 @@ export default function Home() {
     }
   }
 
-  const sendNotChangeState = async () => {
-    const CCR: TransactionRequestSuave = {
-      confidentialInputs: '0x',
-      kettleAddress: '0xB5fEAfbDD752ad52Afb7e1bD2E40432A485bBB7F',
-      to: deployedAddress,
-      gasPrice: parseGwei('0.0000000002'), 
-      gas: parseGwei('0.0002'),
-      type: SuaveTxTypes.ConfidentialRequest, 
-      chainId: 16813125,
-      data: '0xc0473c8100000000000000000000000000000000000000000000000000000000', // calling nilExample()
-      nonce
-    };
-    if (account) {
-      const wallet = createWalletClient({ 
-        account, 
-        transport: http(suaveUrl)
-      });
-      const serialized = serializeTransaction(CCR);
-      const serializedHash = keccak256(serialized);
-      const hexSignature = await (window as any).ethereum.request({ method: 'eth_sign', params: [account, serializedHash] });
-      const signature = hexToSignature(hexSignature);
-      const serializedSignedTx = serializeTransaction(CCR, signature);
-      const hash = await wallet.sendRawTransaction({
-          serializedTransaction: serializedSignedTx as `0x${string}`
-      });
-      console.log(`Transaction hash: ${hash}`);
-      nonce++;
-    }
+  const sendNilExample = async () => {
+    alert("A confidential request fails if it tries to modify the state directly.")
   }
 
   const fetchState = async () => {
@@ -150,23 +134,13 @@ export default function Home() {
           <div className="relative flex my-8">
               <p><b>Your balance:</b> {balance}</p>
           </div>
-          {balance === '0'&& (
-            <div className="relative flex my-8">
-                <button 
-                  className='border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'
-                  onClick={() => getFunds()}
-                  >
-                    Get Funds
-                </button>
-            </div>
-          )}
           {account && (
             <div className="relative flex my-8">
                 <button 
                   className='border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'
                   onClick={() => getFunds()}
                   >
-                    Get More Funds
+                    Get Funds
                 </button>
             </div>
           )}
@@ -182,21 +156,21 @@ export default function Home() {
             <div>
               <div className='flex flex-col col-2 md:flex-row'>
                 <div className='border border-gray-300 rounded-xl mx-2 my-4 p-4 w-full'>
-                  <p className='text-l font-bold'>Change state</p>
+                  <p className='text-l font-bold'>Use callback</p>
                   <button 
                     className='mt-4 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'
-                    onClick={() => sendChangeState()}
+                    onClick={() => sendExample()}
                     >
-                      Send
+                      example()
                   </button>
                 </div>
                 <div className='border border-gray-300 rounded-xl mx-2 my-4 p-4 w-full'>
-                <p className='text-l font-bold'>Same state</p>
+                <p className='text-l font-bold'>Change directly</p>
                   <button 
                   className='mt-4 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'
-                  onClick={() => sendNotChangeState()}
+                  onClick={() => sendNilExample()}
                   >
-                    Send
+                    nilExample()
                   </button>
                 </div>
               </div>
@@ -211,6 +185,10 @@ export default function Home() {
             </div>
           )}
         </div>
+      </div>
+
+      <div>
+        <p>{hash}</p>
       </div>
 
       <Links />
